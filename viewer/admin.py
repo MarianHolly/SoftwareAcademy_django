662a2b60fb343b1a56407ac05c655a7a7b191416
Stupel <marian.holly7117@gmail.com>
2025-03-24 20:47:15 +0100

    Models 'Creator' a 'Movie'

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from viewer.models import Country, Genre
+from viewer.models import Country, Creator, Genre, Movie
 
 # Register your models here.
 admin.site.register(Country)
+admin.site.register(Creator)
 admin.site.register(Genre)
+admin.site.register(Movie)
